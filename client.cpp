d8f66e4dadc7f6ab354188eb66b32889ca323c04
Peiru-Elaine <1224037466@qq.com>
2022-02-15 22:58:55 -0500

    2021_2_15

--- client.cpp
+++ client.cpp
@@ -11,7 +11,7 @@
 #include <vector>
 #include <iostream>
 void Client::proxy_connet_server(std::string proxy_send_server, int accept_fd){
-    
+    //size_t recv_resp=0;
     std::cout<<"client: "<<sockfd<<std::endl;
     int _connect = connect(sockfd, addr_info_list->ai_addr, addr_info_list->ai_addrlen);
     if(_connect==-1){
@@ -25,25 +25,29 @@ void Client::proxy_connet_server(std::string proxy_send_server, int accept_fd){
     }
     //可能有问题
     //char proxy_recv[10000] = "";
+    char proxy_recv[65536] = "";
+    //proxy_recv_mssg.resize(65535);
+    //recv_resp = 0;
     while(1){
-        proxy_recv_mssg.resize(65535);
-        recv_resp = recv(sockfd, proxy_recv_mssg.data(), proxy_recv_mssg.size(),0);
+        //proxy_recv_mssg.resize(65535);
+        recv_resp = recv(sockfd, proxy_recv, 65536,0);
+        std::cout<<recv_resp<<std::endl;
         if(recv_resp==-1){
             perror("Fail to receive the message from the server\n");
             exit(EXIT_FAILURE);
         }
-        if(recv_resp==0){
+        if(recv_resp<=0){
             break;
         }
-        
-        if(send(accept_fd, proxy_recv_mssg.data(), recv_resp, 0)==-1){
+        proxy_recv_mssg.insert(proxy_recv_mssg.end(), proxy_recv, proxy_recv+recv_resp);
+        if(send(accept_fd, proxy_recv, recv_resp, 0)==-1){
             perror("Proxy fail to send message to the client\n");
             exit(EXIT_FAILURE);
         }
     }
-   
     
-    //std::cout<<recv_resp<<std::endl;
+    std::cout<<proxy_recv_mssg.data()<<std::endl;
+    std::cout<<errno<<std::endl;
 }
 
 /*void Client::proxy_client(int accept_fd){
@@ -59,3 +63,4 @@ Client::~Client(){}
 
 
 //Content-Length
+
